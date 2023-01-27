1e385188401654cfbb3b31af5c28355202d41c79
Bhagirath Saxena <bk2358678@gmail.com>
2023-01-27 14:41:17 +0530

    error fix c99.py

--- c99.py
+++ c99.py
@@ -7,19 +7,16 @@ def get_subdomains():
     domains = sys.stdin.read().splitlines()
     for domain in domains:
         site_url = f"https://subdomainfinder.c99.nl/search.php?domain={domain}"
-        response = requests.get(site_url).text
-        soup = BeautifulSoup(response, "html.parser")
-        a = soup.findAll("a", class_="text-decoration-none")
+        response = requests.get(site_url)
+        soup = BeautifulSoup(response.text, "html.parser")
+
         for a in soup.find_all('a', href=True):
-            h = a['href']
-            if re.search("scans", h):
-                url = "https:" + h
-                response = requests.get(url).text
-                s = BeautifulSoup(response, "html.parser")
-                tab = s.find('table', {'id': 'result_table'})
-                links = s.findAll("a")
-                for link in links:
-                    check = link.getText()
-                    if check.endswith(f"{domain}"):
-                        print(check)
+            if re.search("scans", a['href']):
+                url = "https:" + a['href']
+                response = requests.get(url)
+                soup = BeautifulSoup(response.text, "html.parser")
+                tabs = soup.find_all(class_="link")
+                for tab in tabs:
+                    if re.search(f"{domain}", tab.text):
+                        print(tab.text)
 get_subdomains()
