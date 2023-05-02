20f99301c30243fb68d78352e204338e025229a5
Bhagirath Saxena <72344025+rix4uni@users.noreply.github.com>
2023-05-02 05:41:35 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,34 +2,6 @@
 
 eval "pip3 install -r requirements.txt"
 
-echo -e "Checking Golang latest version"
-go_latest_version=$(curl -L -s https://golang.org/VERSION?m=text)
-go_system_version=$(go version | awk {'print $3'})
-
-if [[ "$go_latest_version" == "$go_system_version" ]]; then
-    echo -e "Golang is already installed and updated"
-elif [[ "$go_latest_version" != "$go_system_version" ]]; then
-    echo -e "Installing Golang latest version"
-    eval wget https://dl.google.com/go/${go_latest_version}.linux-amd64.tar.gz
-    sudo tar -C /usr/local -xzf ${go_latest_version}.linux-amd64.tar.gz && rm -rf ${go_latest_version}.linux-amd64.tar.gz
-    export GOROOT=/usr/local/go
-    export GOPATH=$HOME/go
-    export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
-    if [[ -f ~/.zshrc ]]; then
-        echo -e "\n#Golang Variable" >> ~/.zshrc
-        echo 'export GOROOT=/usr/local/go' >> ~/.zshrc
-        echo 'export GOPATH=$HOME/go' >> ~/.zshrc
-        echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.zshrc
-        eval source ~/.zshrc
-    elif [[ -f ~/.bashrc ]]; then
-        echo -e "\n#Golang Variable" >> ~/.bashrc
-        echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
-        echo 'export GOPATH=$HOME/go' >> ~/.bashrc
-        echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bashrc
-        eval source ~/.bashrc
-    fi
-fi
-
 # install some tools
 if [[ ! -x "$(command -v jq)" ]]; then
 	printf "${BBLUE}Installing jq\n"
