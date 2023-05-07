06779079347f290f28ffb723d0e8376571e589ca
Bhagirath Saxena <72344025+rix4uni@users.noreply.github.com>
2023-05-07 11:57:36 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,15 +3,6 @@
 eval "pip3 install -r requirements.txt"
 
 # install some tools
-if [[ ! -x "$(command -v jq)" ]]; then
-	printf "${BBLUE}Installing jq\n"
-	apt install jq -y &>/dev/null;
-elif [[ ! -x "$(command -v anew)" ]]; then
-	printf "${BBLUE}Installing anew\n"
-	go install -v github.com/tomnomnom/anew@latest &>/dev/null;
-elif [[ ! -x "$(command -v unfurl)" ]]; then
-	printf "${BBLUE}Installing unfurl\n"
-	go install github.com/tomnomnom/unfurl@latest &>/dev/null;
-fi
-
-cp -r ../SubDog/ /usr/bin/
+apt install jq -y
+go install -v github.com/tomnomnom/anew@latest
+go install github.com/tomnomnom/unfurl@latest
