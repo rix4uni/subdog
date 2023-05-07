d4ecd49eff29e965a76eb17df278855e927b1a7d
Bhagirath Saxena <bk2358678@gmail.com>
2023-05-07 12:36:31 +0530

    Update

--- install.sh
+++ install.sh
@@ -6,3 +6,9 @@ eval "pip3 install -r requirements.txt"
 apt install jq -y
 go install -v github.com/tomnomnom/anew@latest
 go install github.com/tomnomnom/unfurl@latest
+
+# setup
+mkdir ~/bin
+mv SubDog ~/bin
+chmod +x ~/bin/SubDog/*
+if ! grep -qxF 'export PATH="$HOME/bin/SubDog:$PATH"' ~/.bashrc ; then echo -e '\nexport PATH="$HOME/bin/SubDog:$PATH"' >> ~/.bashrc ; fi
